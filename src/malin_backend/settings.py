db3309fcefb154d2e85887f05c2d622ffd453286
Benjamin Nyakambangwe <benjmainnyakambangwe@gmail.com>
2022-05-01 07:58:02 +0200

    changed mail sett and db to ignore

--- src/malin_backend/settings.py
+++ src/malin_backend/settings.py
@@ -127,8 +127,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'malingreatsdev@gmail.com'
+EMAIL_HOST_PASSWORD = '123456seven'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
